ee477656680c910e08b464daa12c1c7ec161dbbb
Aleksei <kovrigin@inetris.ru>
2022-05-02 15:05:13 +0300

    first version

---
+++ index.bat
@@ -0,0 +1,2 @@
+@echo OFF
+"D:\Programs\Php\php.exe" D:\Programs\OpenServer\domains\temp\index.php %*
--- index.php
+++ index.php
@@ -1,1 +1,23 @@
+#!/usr/bin/env
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use classmap\Lib as Lib;
+
+foreach ($argv as $item){
+	parse_str($item, $output);
+	if (isset($output['--file'])){
+		$fp = @fopen($output['--file'], "r");
+		if ($fp) {
+			while (($buffer = fgets($fp, 4096)) !== false) {
+				$e = new Lib;
+				$e1 = $e->example($buffer);
+				echo $e1;
+			}
+			if (!feof($fp)) {
+				echo "Ошибка: fgets() неожиданно потерпел неудачу\n";
+			}
+			fclose($fp);
+		}
+    }
+}
+
